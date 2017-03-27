1ffe369015a4532750b724874716ba1c502e2785
jevgenik <jevgenik@microsoft.com>
2017-03-27 21:56:04 +0300

    requirements fix

--- setup.py
+++ setup.py
@@ -1,4 +1,36 @@
-from distutils.core import setup
+import sys
+
+from os import path as p
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+
+def read(filename, parent=None):
+    parent = (parent or __file__)
+
+    try:
+        with open(p.join(p.dirname(parent), filename)) as f:
+            return f.read()
+    except IOError:
+        return ''
+
+
+def parse_requirements(filename, parent=None):
+    parent = (parent or __file__)
+    filepath = p.join(p.dirname(parent), filename)
+    content = read(filename, parent)
+
+    for line_number, line in enumerate(content.splitlines(), 1):
+        candidate = line.strip()
+
+        if candidate.startswith('-r'):
+            for item in parse_requirements(candidate[2:].strip(), filepath):
+                yield item
+        else:
+            yield candidate
 
 setup(
   name='vallox_websocket_api',
@@ -8,5 +40,6 @@ setup(
   license='LGPL 3',
   author='yozik04',
   author_email='yozik04@gmail.com',
-  description='Vallox WebSocket client'
+  description='Vallox WebSocket client',
+  install_requires=list(parse_requirements('requirements.txt'))
 )
